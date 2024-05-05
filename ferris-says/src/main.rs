c11fd22c0539c3d61410c435c2c8e256e574c379
ticktack-st <moglandwrapper@gmail.com>
2024-05-05 09:14:58 +0900

    add: formatter

--- ferris-says/src/main.rs
+++ ferris-says/src/main.rs
@@ -2,7 +2,7 @@ use ferris_says::say;
 use std::io::{stdout, BufWriter};
 
 fn main() {
-    let stdout =  stdout();
+    let stdout = stdout();
     let message = String::from("Hello fellow Rustaceans!");
     let width = message.chars().count();
 
