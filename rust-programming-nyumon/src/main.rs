ac9654eb2aaeb9cfea0bd86d1e5beb82aa2ab9b7
ticktack-st <moglandwrapper@gmail.com>
2024-05-05 09:48:41 +0900

    test: trait

---
+++ rust-programming-nyumon/src/main.rs
@@ -0,0 +1,40 @@
+fn main() {
+	let dog = Dog{};
+	let cat = Cat{};
+	show_animal_data(dog);
+	show_animal_data(cat);
+}
+
+trait Animal {
+	fn lifespan(&self) -> u32;
+	fn sientific_name(&self) -> String;
+}
+
+struct Dog;
+
+impl Animal for Dog {
+	fn lifespan(&self) -> u32 {
+		13
+	}
+
+	fn sientific_name(&self) -> String {
+		"Canis lupus familiaris".to_string()
+	}
+}
+
+struct Cat;
+
+impl Animal for Cat {
+	fn lifespan(&self) -> u32 {
+		16
+	}
+
+	fn sientific_name(&self) -> String {
+		"Felis catus".to_string()
+	}
+}
+
+fn show_animal_data<T: Animal>(animal: T) {
+	println!("Lifespan: {} years", animal.lifespan());
+	println!("Scientific name: {}", animal.sientific_name());
+}
