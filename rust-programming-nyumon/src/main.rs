24e1abd483d827e6a9d0a6f5c5a734a5376464b0
ticktack-st <moglandwrapper@gmail.com>
2024-05-15 23:18:46 +0900

    test: tokio runtime

--- rust-programming-nyumon/src/main.rs
+++ rust-programming-nyumon/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-	println!("Hello World!");
+async fn add(left: i32, right: i32) -> i32 {
+    let a = left + right;
+	println!("{}", a);
+	left + right
+}
+#[tokio::main]
+async fn main() {
+    add(2, 3).await;
 }
