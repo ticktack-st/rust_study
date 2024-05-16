e23f6a130e1bf7c980a04b301223c009a6ff25c7
ticktack-st <moglandwrapper@gmail.com>
2024-05-16 17:56:55 +0900

    test: clap@3.0.0

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -1,6 +1,19 @@
-use std::env;
+extern crate clap;
+use clap::{arg, App};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let matches = App::new("My RPN program")
+        .version("1.0.0")
+        .author("Your name")
+        .about("Super awesome sample RPN calculator")
+        .arg(arg!([FILE] "Fromulas written in RPN").required(false))
+        .arg(arg!(-v --verbose ... "Sets the level of verbosity").required(false))
+        .get_matches();
+
+    match matches.value_of("FILE") {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file specified."),
+    }
+    let verbose = matches.is_present("verbose");
+    println!("Is verbosity specified?: {}", verbose);
 }
