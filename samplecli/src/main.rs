c74ec956e394bfe3ceef939e4a810a7db8bc30d2
ticktack-st <moglandwrapper@gmail.com>
2024-05-16 18:10:38 +0900

    test: derive macro

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -1,19 +1,28 @@
-extern crate clap;
-use clap::{arg, App};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "Your name",
+    about = "Super awesome sample RPN calculator"
+)]
+struct Opts {
+    /// Sets the level of verbosity
+    #[clap(short, long)]
+    verbose: bool,
+
+    /// Formulas written in RPN
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
 
 fn main() {
-    let matches = App::new("My RPN program")
-        .version("1.0.0")
-        .author("Your name")
-        .about("Super awesome sample RPN calculator")
-        .arg(arg!([FILE] "Fromulas written in RPN").required(false))
-        .arg(arg!(-v --verbose ... "Sets the level of verbosity").required(false))
-        .get_matches();
+    let opts = Opts::perse();
 
-    match matches.value_of("FILE") {
+    match opts.formula_file {
         Some(file) => println!("File specified: {}", file),
         None => println!("No file specified."),
     }
-    let verbose = matches.is_present("verbose");
-    println!("Is verbosity specified?: {}", verbose);
+    println!("Is verbosity specified?: {}", opts.verbose);
 }
